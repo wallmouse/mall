3b5135e0ad223cffab97bcc34487b7842621b1b6
wallmouse <865935990@qq.com>
2019-08-15 10:35:10 +0800

    init

---
+++ src/main/java/com/imooc/mall/test.java
@@ -0,0 +1,9 @@
+package com.imooc.mall;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+@Controller
+public class test {
+}
